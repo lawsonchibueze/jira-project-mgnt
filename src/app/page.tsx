2a292f231ae84ed9d78a3593f5b3efeeae572709
Lawson Chibueze <lawson.chibueze@loctechng.com>
2024-10-22 23:44:24 +0100

    added session middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,35 @@
+"use client"
 
-
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-
 
 export default function Home() {
 
+ const router = useRouter()
+
+ const { data, isLoading} = useCurrent()
+ const {mutate} = useLogout()
+
+ useEffect (() => {
+  if (!data && isLoading) {
+    router.push("/sign-in")
+
+  }
+ }, [data])
+
+ return (
+  <div>
+    Only visible to authorized users
+    <Button onClick={() => mutate()}>
+      Logout
+    </Button>
+
+  </div>
+ )
+
+ 
   
-  return (
-    <div className="">
-      <Input />
-
-      <Button className="primary">
-       Primary 123
-      </Button>
-      <Button variant="secondary">
-       Secondary
-      </Button>
-      <Button variant="destructive">
-       Destructive
-      </Button>
-      <Button variant="ghost">
-       Ghost
-      </Button>
-      <Button variant="muted">
-       Muted
-      </Button>
-      <Button variant="outline">
-       Outline
-      </Button>
-      <Button variant="teritary">
-       Teritary
-      </Button>
-
-    </div>
-  );
 }
