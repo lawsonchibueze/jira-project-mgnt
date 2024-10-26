005972fee856a6017988d63546012c46e176d66d
Lawson Chibueze <lawson.chibueze@loctechng.com>
2024-10-26 20:42:06 +0100

    workspace switcher

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -2,14 +2,18 @@ import Image from "next/image"
 import Link from "next/link"
 import { DottedSeparator } from "./dotted-separator"
 import { Navigation } from "./navigation"
+import { WorkspaceSwitcher } from "./workspace-switcher"
 
 export const Sidebar = () => {
     return (
         <aside className="h-full bg-neutral-100 p-4 w-full">
             <Link href="/">
-            <Image  src="/logo5.webp" alt="logo" width={164} height={48}/>
+            <Image  src="/logo5.webp" alt="logo" width="164" height="48"/>
             
             </Link>
+            <DottedSeparator className="my-4" />
+            <WorkspaceSwitcher />   
+
             <DottedSeparator className="my-4" />
             <Navigation />
 
--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -7,6 +7,17 @@ import { ID } from "node-appwrite";
 
 const app = new Hono()
 
+.get("/", sessionMiddleware, async (c) => {
+    const databases = c.get("databases")
+
+    const workspaces = await databases.listDocuments(
+        DATABASE_ID,
+        WORKSPACES_ID
+
+    );
+    return c.json({data:workspaces})
+})
+
 .post(
     "/",
     zValidator("form", createWorkspaceSchema),
