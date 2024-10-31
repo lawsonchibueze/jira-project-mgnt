ab50f9d248bdaac55159b0e72f8a4f2a0ddd9ceb
Lawson Chibueze <lawson.chibueze@loctechng.com>
2024-10-31 09:37:51 +0100

    added deleting and updating workspaces

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
 	"./src/features/**/*.{js,ts,jsx,tsx,mdx}",
+	"./src/hooks/**/*.{js,ts,jsx,tsx,mdx}",
 	
   ],
   theme: {
@@ -60,6 +61,7 @@ const config: Config = {
   		}
   	}
   },
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
   plugins: [require("tailwindcss-animate")],
 };
 export default config;
